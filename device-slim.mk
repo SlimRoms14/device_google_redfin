17a5b3f2574c8f3ee0eb629ee8a79f25e03b6deb
Rashed Abdel-Tawab <rashed@linux.com>
2019-11-20 10:22:06 -0500

    redfin: Initial Lineage bringup
    Change-Id: Ieddd0460eeb223512b5d3ce9504f6a654d44c0d9

---
+++ device-slim.mk
@@ -0,0 +1,7 @@
+#
+# Copyright (C) 2023 The SlimRoms Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/google/redbull/device-slim.mk)
---
+++ slim_redfin.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The SlimRoms Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit some common SlimRoms stuff.
+TARGET_DISABLE_EPPE := true
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/redfin/aosp_redfin.mk)
+
+include device/google/redfin/device-slim.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 5
+PRODUCT_NAME := slim_redfin
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_PRODUCT=redfin \
+    PRIVATE_BUILD_DESC="redfin-user 14 UP1A.231105.001 10817346 release-keys"
+
+BUILD_FINGERPRINT := google/redfin/redfin:14/UP1A.231105.001/10817346:user/release-keys
