3c12cb0d7ac04c9f4447ce25c8fa4fcd16a1e887
Michael Bestas <mkbestas@gmail.com>
2022-05-04 20:11:46 +0300

    Convert HbmSVManager overlays to RRO
    Auto generated RRO is not working here because we don't actually have
    access to vendor/google_devices/sunfish/proprietary/display/HbmSVManager
    
    Change-Id: I54d885c233151498372e1b510284d6a297ac3e94

--- device-slim.mk
+++ device-slim.mk
@@ -9,5 +9,9 @@ $(call inherit-product, device/google/redbull/device-slim.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/redfin/overlay-slim
 
+# HBM
+PRODUCT_PACKAGES += \
+    HbmSVManagerOverlay
+
 # wireless_charger HAL service
 include device/google/redfin/wireless_charger/wireless_charger.mk
